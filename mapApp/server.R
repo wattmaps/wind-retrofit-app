ddb757133d9e77eef67e74318df071eb4b412833
colleenmccamy <colleenrmccamy@gmail.com>
2023-04-30 16:31:52 -0700

    created test map and graph

--- mapApp/server.R
+++ mapApp/server.R
@@ -1,2 +1,6 @@
 # server instructions ----
-server <- function(input, output) {}
+server <- function(input, output) {
+  
+  # automatically stopping the app when browser is closed
+  session$onSessionEnded(stopApp)
+}
